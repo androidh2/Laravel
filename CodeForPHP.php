f925418f56e012d1f8c9aab4c7beb3d5230d2f09
androidh2 <32007215+androidh2@users.noreply.github.com>
2019-08-14 19:28:05 +0900

    Update CodeForPHP.php

--- CodeForPHP.php
+++ CodeForPHP.php
@@ -1,11 +1,11 @@
 <?php
 if ($item['class']==0) {
   echo "<li>";
-  echo "<span>".$item['disp']."</span>"
+  echo "  <span>".$item['disp']."</span>"
   echo "</li>"
 } else {
   echo "<li>";
-  echo "<a href=".$item['link'].">".$item['disp']."</a>"
+  echo "  <a href=".$item['link'].">".$item['disp']."</a>"
   echo "</li>"
 }
 ?>
