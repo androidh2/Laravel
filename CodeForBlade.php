3b36e481f51d238d360399543a21aafa2bab166c
androidh2 <32007215+androidh2@users.noreply.github.com>
2019-08-14 19:55:54 +0900

    Update CodeForBlade.php

--- CodeForBlade.php
+++ CodeForBlade.php
@@ -1,10 +1,6 @@
 <div>Code For Blade</div>
 @if ($item['class']==0)
-  <li>
-  <span>{{$item['disp']}}</span>
-  </li>
+  <li><span>{{$item['disp']}}</span></li>
 @else
-  <li>
-  <a href="{{$item['link']}}">{{$item['disp']}}</a>
-  </li>
+  <li><a href="{{$item['link']}}">{{$item['disp']}}</a></li>
 @endif
